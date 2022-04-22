796243c23f1963fe8d1d94efab0b94f0b417be78
EmielCallens <emielcallens@telenet.be>
2022-04-22 16:44:54 +0200

    Join tutorial files into 1

--- python_tutorial.py
+++ python_tutorial.py
@@ -1,8 +1,62 @@
-'''test test test'''
-print("test")
-value = 5
-#testing
-#changed folder
-value1 = 10
-print(value)
-#test
+import sys
+import math
+import random
+import threading
+import time
+from functools import reduce
+'''
+print("5 + 2 =", 5+2)
+print("5 - 2 =", 5-2)
+print("5 / 2 =", 5/2)
+print("5 % 2 =", 5%2)
+print("5 ** 2 =", 5**2)
+print("5 // 2 =", 5//2)
+i1 = 2
+i1 += 1
+print("i1", i1)
+print("Random", random.randint(1, 101))
+#NaN and inf
+<>, <=, >=, ==, !=
+print("Enter age: ", end="")
+age = int(input())
+
+if age > 18:
+    print("you can drink liquor")
+elif age >= 16:
+    print("you can drink beer")
+# and or not
+canVote = True if age >= 18 else False
+print(canVote)
+
+print(r"I'll be ignored \n")
+print("Hello "+ "You")
+str3 = "Hello You"
+print("Length", len(str3))
+print("1st", str3[0])
+print("Last", str3[-1])
+print("1st 3", str3[0:3])
+print("Every other", str3[0:-1:2])
+str3 = str3.replace("Hello", "Goodbye")
+print(str3)
+str3 = str3[:8] + "y" + str3[9:]
+print(str3)
+print("you" not in str3)
+print("You index", str3.find("you"))
+print("    Hello    ".strip()) #lstrip rstrip
+print(" ".join(["Some", "Words"]))
+print("A string".split(" "))
+
+int1 = int2 = 5
+print(f"{int1}+{int2} = {int1 + int2}")
+
+print("A String".lower())
+print("A String".upper())
+print("A String 123".isalnum())
+print("AString123".isalnum())
+print("A String".isalpha())
+print("AString".isalpha())
+print("A String".isdigit())
+print("123".isdigit())
+'''
+
+
