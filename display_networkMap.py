cae2ef8a6902029f3538f986c15a7b3dc8c9e9e4
EmielCallens <emielcallens@telenet.be>
2022-04-24 21:52:51 +0200

    reworked networkMap for DictWrite and Read with header and generated some new map files

--- display_networkMap.py
+++ display_networkMap.py
@@ -1,10 +1,47 @@
 # GUI display of the generated network map
-'''
+"""
 Possible extension later to show network traffic data after simulation
     - Add what links are used and amount of packages send and lost over this link
     - Add Energy used value of each node
-'''
+"""
+import csv
+from lib import nodes
+# import for gui development
+from tkinter import *
+from tkinter import ttk
+# import for graph plotting -  why is this not included?
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Read networkMap file
-file_networkMap = "networkMap.txt"
+file_networkMap = "networkMap/networkMap_n50_min100_area3000x3000_id0.csv"
+dict_networkNodes = {}
+with open(file_networkMap, newline='') as csvfile:
+    csvReader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
+    for row in csvReader:
+        #print(row['id'], row['x'], row['y'])
+        dict_networkNodes[row['id']] = nodes.NetworkNode(row['id'], row['x'], row['y'])
+        print(dict_networkNodes[row['id']].id, dict_networkNodes[row['id']].x, dict_networkNodes[row['id']].y)
+
 # Read simulationData file
+"""
+    For future stuff after I have writen the simulation file
+"""
+
+class Topology:
+    def __init__(self, root):
+        self.entry_value = StringVar(root, value="")
+        root.title("Topology")
+        root.geometry("600x600")
+        root.resizable(width=False, height=False)
+
+        # Do I really need this?
+        style = ttk.Style()
+        style.configure("TButton", font="Serif 15", padding=10)
+        style.configure("TEntry", font="Serif 18", padding=10)
+        self.number_entry = ttk.Entry(root, textvariable=self.entry_value, width=50)
+        self.number_entry.grid(row=0, columnspan=4)
+
+
+root = Tk()
+root.mainloop()
--- gen_networkMap.py
+++ gen_networkMap.py
@@ -1,4 +1,3 @@
-
 """
     - Add multiple sinks? define how many sinks, max of one on each available spot (9)
         - Sinks don't get a normal id but are sid = s0, s1, s2, ...
@@ -11,44 +10,22 @@ import math
 import random
 import csv
 from os.path import exists as file_exists
+from lib import nodes
 
 # Number of network nodes
-iNodes = 50
+iNodes = 200
 
 # Distances in meters
-minDistance = 50
+minDistance = 100
 networkX = 3000
 networkY = 3000
 
 # Should I do this with databases for later statistics?
 # Let's do it with an object for now
+dict_nodes = {0: nodes.NetworkNode(0, networkX / 2, networkY / 2)}
 
-
-class NetworkNode:
-    def __init__(self, node_id=-1, x=0.0, y=0.0):
-        self.__nodeId = node_id
-        self.__x = x
-        self.__y = y
-
-    @property
-    def id(self):
-        return self.__nodeId
-
-    @property
-    def x(self):
-        return self.__x
-
-    @property
-    def y(self):
-        return self.__y
-
-    def __str__(self):
-        return "ID:{} [x:{}, y:{}]".format(self.__nodeId, self.__x, self.__y)
-
-
-nodes = {0: NetworkNode(0, networkX/2, networkY/2)}
-print(nodes[0].x)
-for i in range(1, iNodes+1):
+print(dict_nodes[0].x)
+for i in range(1, iNodes + 1):
     co_accept = False
     new_x = 0.0
     new_y = 0.0
@@ -58,12 +35,12 @@ for i in range(1, iNodes+1):
         new_y = random.uniform(0, networkY)
 
         # code to enforce min distance to other nodes.
-        if len(nodes) != 0:
+        if len(dict_nodes) != 0:
             co_accept = True
 
-            for j in nodes:
-                distance_x = new_x - nodes[j].x
-                distance_y = new_y - nodes[j].y
+            for j in dict_nodes:
+                distance_x = new_x - dict_nodes[j].x
+                distance_y = new_y - dict_nodes[j].y
                 distance_direct = math.sqrt(distance_x ** 2 + distance_y ** 2)
 
                 # distance not long enough with a neighbor node, repeat while loop
@@ -74,32 +51,31 @@ for i in range(1, iNodes+1):
         else:
             co_accept = True
 
-    new_node = NetworkNode(i, new_x, new_y)
-    nodes[i] = new_node
+    new_node = nodes.NetworkNode(i, new_x, new_y)
+    dict_nodes[i] = new_node
     # print(nodes[i])
 
 # Store Nodes on csv file for later use
 file_itr = 0
-fileName = "networkMap/networkMap_n{}_min{}_area{}x{}_id{}.csv"\
+fileName = "networkMap/networkMap_n{}_min{}_area{}x{}_id{}.csv" \
     .format(iNodes, minDistance, networkX, networkY, file_itr)
 
 # change id until file doesn't exist to prevent overwriting existing networkMaps
 while file_exists(fileName):
     file_itr += 1
-    fileName = "networkMap/networkMap_n{}_min{}_area{}x{}_id{}.csv"\
+    fileName = "networkMap/networkMap_n{}_min{}_area{}x{}_id{}.csv" \
         .format(iNodes, minDistance, networkX, networkY, file_itr)
 
 with open(fileName, mode="w", newline='') as csvfile:
-    csvWriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    # csvWriter.writerow(['ID'] + ['x'] + ['y'])
-    csvWriter.writerow(['ID', 'x', 'y'])
-    for i in nodes:
-        csvWriter.writerow([nodes[i].id, nodes[i].x, nodes[i].y])
-        # file.write("{};{};{}\n".format(nodes[i].id, nodes[i].x, nodes[i].y))
+    fieldnames = ['id', 'x', 'y']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    writer.writeheader()
+    for i in dict_nodes:
+        writer.writerow({'id': dict_nodes[i].id, 'x': dict_nodes[i].x, 'y': dict_nodes[i].y})
 
 with open(fileName, newline='') as csvfile:
-    csvReader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in csvReader:
+    reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
+    for row in reader:
         print(row)
 
 print(csvfile.closed)
