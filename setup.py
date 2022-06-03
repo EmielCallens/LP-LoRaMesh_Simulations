064a4f0aa062ce303cfa09747f96b93e8aef9bf7
EmielCallens <emielcallens@telenet.be>
2022-06-03 23:07:23 +0200

    set preamble and cycle time back to normal

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ class Sim1:
     @staticmethod
     def n_cycle():
         # return 13402  # sample period, in symbols
-        return 1002
+        return 13402
 
     @staticmethod
     def n_preamble():
         # return 13404  # preamble length in symbols
-        return 1004
+        return 13404
 
     @staticmethod
     def target():
